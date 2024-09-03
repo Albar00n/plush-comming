422605febd70fc059ce77212905956bff0b1c22b
Albar00n <mgurshi73@gmail.com>
2024-09-03 12:56:10 +0400

    a

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<div>
+			<iframe
+				src="https://theplushsilk-cde5d.web.app/"
+				title="the plush silk page"
+				className="h-[100vh] w-full"
+			/>
+		</div>
+	);
+}
